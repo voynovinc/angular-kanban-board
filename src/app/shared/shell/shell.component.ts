8e3ac2652edc32b9ff878953d6e5bedbd72d7bd6
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-10 20:02:38 -0500

    write shared/shell functionality

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-shell',
-  templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.scss']
+  selector: "app-shell",
+  templateUrl: "./shell.component.html",
+  styleUrls: ["./shell.component.scss"]
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
