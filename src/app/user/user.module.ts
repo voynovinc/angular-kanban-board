b93c412f1595f9c8978c1c7132df46519297c68f
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-11 17:34:40 -0500

    Add imports to user.module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 
 import { UserRoutingModule } from "./user-routing.module";
 import { SharedModule } from "../shared/shared.module";
-import { LoginPageComponent } from './login-page/login-page.component';
-import { GoogleSigninDirective } from './google-signin.directive';
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { GoogleSigninDirective } from "./google-signin.directive";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [LoginPageComponent, GoogleSigninDirective],
-  imports: [CommonModule, UserRoutingModule, SharedModule]
+  imports: [CommonModule, UserRoutingModule, SharedModule, ReactiveFormsModule]
 })
 export class UserModule {}
