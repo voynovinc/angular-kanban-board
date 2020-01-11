6baee8c77f61546ed659b0772f60c416dfbbc174
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-11 07:18:21 -0500

    Add user/login-page component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 
 import { UserRoutingModule } from "./user-routing.module";
 import { SharedModule } from "../shared/shared.module";
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginPageComponent],
   imports: [CommonModule, UserRoutingModule, SharedModule]
 })
 export class UserModule {}
