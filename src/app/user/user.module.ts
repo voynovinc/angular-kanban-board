90fff629a30f664dc5d50969fc2eb19ce8223209
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-11 07:13:30 -0500

    Add shared.module to user.module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { UserRoutingModule } from './user-routing.module';
-
+import { UserRoutingModule } from "./user-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  imports: [CommonModule, UserRoutingModule, SharedModule]
 })
-export class UserModule { }
+export class UserModule {}
