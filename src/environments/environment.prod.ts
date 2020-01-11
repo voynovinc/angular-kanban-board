240d6d3f7f2c6e20ddb750a09864644a0ae221f6
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-10 22:00:26 -0500

    Add firebaseConfig to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBa6gaCyKpnn3jkGzuLhHPuMm_ODRe-CVQ",
+    authDomain: "firestarter-demo-3c120.firebaseapp.com",
+    databaseURL: "https://firestarter-demo-3c120.firebaseio.com",
+    projectId: "firestarter-demo-3c120",
+    storageBucket: "firestarter-demo-3c120.appspot.com",
+    messagingSenderId: "943791726198",
+    appId: "1:943791726198:web:0fffe5798c80b34f2bc957"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBa6gaCyKpnn3jkGzuLhHPuMm_ODRe-CVQ",
+    authDomain: "firestarter-demo-3c120.firebaseapp.com",
+    databaseURL: "https://firestarter-demo-3c120.firebaseio.com",
+    projectId: "firestarter-demo-3c120",
+    storageBucket: "firestarter-demo-3c120.appspot.com",
+    messagingSenderId: "943791726198",
+    appId: "1:943791726198:web:0fffe5798c80b34f2bc957"
+  }
 };
 
 /*
