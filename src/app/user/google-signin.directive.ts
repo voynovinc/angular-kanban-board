6ffadfd49e5c6643688a6aa837316e2e711df582
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-11 15:53:12 -0500

    Add google-signin directive to user.module

---
+++ src/app/user/google-signin.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appGoogleSignin]'
+})
+export class GoogleSigninDirective {
+
+  constructor() { }
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from "@angular/common";
 import { UserRoutingModule } from "./user-routing.module";
 import { SharedModule } from "../shared/shared.module";
 import { LoginPageComponent } from './login-page/login-page.component';
+import { GoogleSigninDirective } from './google-signin.directive';
 
 @NgModule({
-  declarations: [LoginPageComponent],
+  declarations: [LoginPageComponent, GoogleSigninDirective],
   imports: [CommonModule, UserRoutingModule, SharedModule]
 })
 export class UserModule {}
