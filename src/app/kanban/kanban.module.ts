de0f78ad49ffc505f282551abe880d7eed959379
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-12 11:14:18 -0500

    Add import modules to kanban.module

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
-import { KanbanRoutingModule } from './kanban-routing.module';
-
+import { KanbanRoutingModule } from "./kanban-routing.module";
+import { SharedModule } from "../shared/shared.module";
+import { FormsModule } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    KanbanRoutingModule
+    KanbanRoutingModule,
+    SharedModule,
+    FormsModule,
+    DragDropModule,
+    MatDialogModule,
+    MatButtonToggleModule
   ]
 })
-export class KanbanModule { }
+export class KanbanModule {}
