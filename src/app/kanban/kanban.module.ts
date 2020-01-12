97c62dd5f094991560fc187d02d3516a573b04ae
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-12 11:08:08 -0500

    Add kanban module

---
+++ src/app/kanban/kanban.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { KanbanRoutingModule } from './kanban-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    KanbanRoutingModule
+  ]
+})
+export class KanbanModule { }
