ddbf258627f6294953f3188eb4d93f9a49e06904
Ivan Voynov <ivanvoinov@hotmail.com>
2020-01-12 10:37:35 -0500

    Create snack service

---
+++ src/app/services/snack.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: "root"
+})
+export class SnackService {
+  constructor(private snackBar: MatSnackBar, private router: Router) {}
+
+  authError() {
+    this.snackBar.open("You must be logged in!", "OK", {
+      duration: 5000
+    });
+
+    return this.snackBar._openedSnackBarRef
+      .onAction()
+      .pipe(tap(_ => this.router.navigate(["/login"])))
+      .subscribe();
+  }
+}
