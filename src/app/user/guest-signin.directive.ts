a5b98e7a7f0af8dea1556322e7b5fbcb129292d7
Ivan Voynov <ivanvoynov@hotmail.com>
2020-01-25 14:50:13 -0500

    Add guest signin functionality

--- src/app/user/guest-signin.directive.ts
+++ src/app/user/guest-signin.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Directive({
-  selector: '[appGuestSignin]'
+  selector: "[appGuestSignin]"
 })
 export class GuestSigninDirective {
+  constructor(private afAuth: AngularFireAuth) {}
 
-  constructor() { }
-
+  @HostListener("click")
+  onClick() {
+    this.afAuth.auth.signInAnonymously().then(user => {
+      console.log("User logged in as anon? " + user.user.isAnonymous);
+    });
+  }
 }
